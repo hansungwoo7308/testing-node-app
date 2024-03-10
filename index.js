5f69860a943c5a17e95bff9382fedd51894aacf2
hansungwoo7308 <hansungwoo7308@gmail.com>
2024-03-10 16:30:40 +0900

    update

--- index.js
+++ index.js
@@ -18,14 +18,19 @@ dotenv.config();
 connectDB();
 
 // middlewares
-app.use(cors(corsSettings));
-app.use(express.json());
+app.use(cors(corsSettings)); // 크로스오리진 보안정책을 설정한다.
+app.use(express.json()); // 클라이언트 요청 데이터를 자바스크립트 형식으로 파싱한다.
+// app.use(express.urlencoded()) // 클라이언트 요청 중에서 폼데이터를 자바스크립트 형식으로 파싱한다.
 
 // static routes
 app.use(express.static(path.join(__dirname, "../client/build")));
+app.use("/", (req, res) => {
+  console.log("home".gray);
+  res.status(200).send("<html>home page</html>");
+});
 app.use("/dashboard", (req, res) => {
-  console.log("some".gray);
-  res.status(200).send("<html>ssdfsdfkshdfl</html>");
+  console.log("dashboard".gray);
+  res.status(200).send("<html>dashboard page</html>");
 });
 
 // dynamic routes
--- routes/api/public_data.js
+++ routes/api/public_data.js
@@ -12,6 +12,7 @@ router.get("/", async (req, res) => {
   // console.log({ collection });
 
   const documents = await collection.find().toArray();
+  // collection.find() 는 커서를 리턴한다. 커서는 포인터다. 유의미한 데이터를 갖기 위해서는 배열로 캐스팅한다.
   console.log({ documents });
 
   res.status(200).json({ documents });
