00780aafe132bbb6f5e3ee4bc19670f8794aa6f4
hansungwoo7308 <hansungwoo7308@gmail.com>
2024-03-12 19:29:04 +0900

    update

--- routes/api/public_data.js
+++ routes/api/public_data.js
@@ -1,20 +1,35 @@
+// const express = require("express");
+// const router = express.Router();
+
+// router.get("/", async (req, res) => {
+//   // sellecting the collection
+//   const collection = require("mongoose").connection.collection("commercial_analysis_service");
+
+//   try {
+//     const documents = await collection.find().toArray();
+//     res.status(200).json({ documents });
+//   } catch (error) {
+//     res.status(500).json({ message: "연결에러..." });
+//   }
+//   // collection.find() 는 커서를 리턴한다. 커서는 포인터다. 유의미한 데이터를 갖기 위해서는 배열로 캐스팅한다.
+// });
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 router.get("/", async (req, res) => {
-  // sellecting the collection
-  require("../../config/connectDB")();
-  const collection = require("mongoose").connection.collection("commercial_analysis_service");
-
   try {
-    const documents = await collection.find().toArray();
+    const documents = await mongoose.connection
+      .collection("commercial_analysis_service")
+      .find()
+      .toArray();
     res.status(200).json({ documents });
   } catch (error) {
-    res.status(500).json({
-      message: "public_data 에서 collection.find().toArray() 에러가 발생",
-    });
+    res.status(500).json({ message: "연결에러..." });
   }
-  // collection.find() 는 커서를 리턴한다. 커서는 포인터다. 유의미한 데이터를 갖기 위해서는 배열로 캐스팅한다.
 });
 
 module.exports = router;
