050b4f168ddde70e99509131ada041952c7d67c1
hansungwoo7308 <hansungwoo7308@gmail.com>
2024-03-12 19:23:27 +0900

    update

--- routes/api/public_data.js
+++ routes/api/public_data.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 
 router.get("/", async (req, res) => {
-  // database
-  const db = mongoose.connection;
-  // console.log({ db });
-
   // sellecting the collection
-  const collection = db.collection("commercial_analysis_service");
-  // console.log({ collection });
+  require("../../config/connectDB")();
+  const collection = require("mongoose").connection.collection("commercial_analysis_service");
 
   try {
     const documents = await collection.find().toArray();
     res.status(200).json({ documents });
   } catch (error) {
-    res.status(500).json({ message: "public_data 에서 collection.find().toArray() 에러가 발생" });
+    res.status(500).json({
+      message: "public_data 에서 collection.find().toArray() 에러가 발생",
+    });
   }
   // collection.find() 는 커서를 리턴한다. 커서는 포인터다. 유의미한 데이터를 갖기 위해서는 배열로 캐스팅한다.
-  // console.log({ documents });
 });
 
 module.exports = router;
